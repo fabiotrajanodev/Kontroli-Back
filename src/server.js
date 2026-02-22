8056a21f85ffef5564474ab2eef1f3d3ba42a7f7
Fabio <suziotrajano@gmail.com>
2026-02-21 23:52:09 -0300

    fix deploy render

--- src/server.js
+++ src/server.js
@@ -43,10 +43,25 @@ fastify.get('/', async (request, reply) => {
 
 
 // Run the server!
-try {
-  await fastify.listen({ port: 3000 })
-} catch (err) {
-  fastify.log.error(err)
-  process.exit(1)
+// try {
+//   await fastify.listen({ port: 3000 })
+// } catch (err) {
+//   fastify.log.error(err)
+//   process.exit(1)
+// }
+
+// Run the server!
+const start = async () => {
+  try {
+    fastify.listen({
+      port: process.env.PORT || 3000,
+      host: "0.0.0.0"
+    })
+    console.log(`Server running`)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
 }
 
+start()
